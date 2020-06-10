cc21d2f453453bb1bf1b2d9424ba0fdec3caeacb
Ashrith Reddy <ashrithreddi81@gmail.com>
2020-06-10 15:23:15 +0530

    update app.js to use latest packages

--- app.js
+++ app.js
@@ -1,67 +1,50 @@
 var request = require('request');
-var prompt = require('prompt');
 var pb = require('pushbullet');
-var cheerio = require('cheerio');
+var HTMLParser  = require('node-html-parser');
 
 // Variables
-var asin, price, pb_token;
-var amzn_url = 'https://www.amazon.in/dp/';
-var span_id = '#actualPriceValue';
-var check_interval = 60000;
-
-var schema = {
-	properties: {
-		asin: {
-			description: 'Enter the product ASIN',
-			type: 'string',
-			required: true
-		},
-		price: {
-			description: 'Enter the desired price (USD)',
-			type: 'number',
-			required: true
-		},
-		pb_token: {
-			description: 'Enter your PushBullet token',
-			type: 'string',
-			required: true,
-			hidden: true
-		}
-	}
-};
-
-prompt.start();
-
-prompt.get(schema, function (error, result) {
-	if (!error) {
-		asin = result.asin;
-		price = result.price;
-		pb_token = result.pb_token;
-
-		amzn_url += asin;
-
-		checkPrice();
-	}
-});
+var price = process.env.PURCHASE_PRICE,
+	anr = process.env.AMAZON_RESOURCE_NAME,
+	pb_token = process.env.PUSH_BULLET_API_TOKEN,
+	check_interval = process.env.NOTIFICATION_INTERVAL_IN_MINUTES;
+var amzn_url = `https://www.amazon.in/dp/${anr}`;
+var span_id = '#priceblock_ourprice';
+var check_interval = check_interval * 60*1000;
 
 function checkPrice() {
 	request(amzn_url, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
-			var $ = cheerio.load(body);
+			var root = HTMLParser.parse(body);
 
-			var list_price = $(span_id).text();
-			var stripped_price = list_price.replace('$', '').replace(',', '');
-
-			if (stripped_price <= price) {
-				sendPush();
+			var list_price = root.querySelector(span_id);
+			if(list_price) {
+				list_price = list_price.text;
+				console.log('Present price: '+list_price);
+				var stripped_price = list_price.replace(/[,₹]+/g, '').trim()
+				if (stripped_price <= price) {
+					sendPush();
+				}
+			} else {
+				sendOutofStock()
 			}
-		}
-		else {
+		} else {
 			console.log("Uh oh. There was an error.");
 		}
 	});
 
 	setTimeout(checkPrice, check_interval);
+}
+
+checkPrice()
+
+function sendOutofStock() {
+	var pusher = new pb(pb_token);
+
+	pusher.note(null, "Product still out of stock", "We are watching for changes in product.", function(error, response) {
+		if(error) {
+			console.log('some error occured:'+error);
+		}
+	});
 }
 
 function sendPush() {
